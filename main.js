5fff6421269a2ce89a329e2b0c9b08e835db02c9
shivenA2023 <124804613+shivenA2023@users.noreply.github.com>
2023-06-12 13:01:06 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//INPUT
+let username = prompt("What is your name?");
+let timeOfDay = prompt("What time of day is it?");
+
+//PROCESS
+let msg = `Hello ${username}! Have a great ${timeOfDay}.`;
+//Output
+alert(msg);
